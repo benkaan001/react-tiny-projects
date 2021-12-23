5105ef6aa17e5281d5e7338f65ae42e090d42ac2
Ben Kaan <benkaan001@gmail.com>
2021-12-22 22:49:06 -0600

    #9-values.js

---
+++ 9-values.js/src/App.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import SingleColor from './SingleColor';
+
+import Values from 'values.js';
+
+function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#6a8ffd').all(8));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    try {
+      let colors = new Values(color).all(8);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className='container'>
+        <h3>values.JS</h3>
+        <form action='' onSubmit={handleSubmit}>
+          <input
+            type='text'
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder='#6a8ffd'
+            className={`${error ? 'error' : null}`}
+          />
+          <button className='btn' type='submit'>
+            submit
+          </button>
+        </form>
+      </section>
+      <section className='colors'>
+        {list.map((color, index) => {
+          console.log(color);
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
